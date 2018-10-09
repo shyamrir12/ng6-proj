e0ad943d000ac1f0212bf58c19cf4df8ef02e945
shyamsir12 <shyamrahangdale38@gmail.com>
2018-10-10 00:13:22 +0530

    last from home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
+
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent,
+    
     HomeComponent
   ],
   imports: [
